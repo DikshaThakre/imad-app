3647594de8e822b9dfed4a3b8bfc4b743c283b6d
DikshaThakre <30907227+DikshaThakre@users.noreply.github.com>
2017-08-16 22:33:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innertHTML='New Value';
+
+//move the image
+var img=document.getElementById('madi');
+img.onclick=function(){
+   img.style.marginLeft='100px'; 
+}
