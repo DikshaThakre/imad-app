1a2aaceec14522fdd5730681e27b090e6b2df9e0
DikshaThakre <30907227+DikshaThakre@users.noreply.github.com>
2017-08-29 20:05:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var config={
 };
 var app = express();
 app.use(morgan('combined'));
--
+app.use(bodyParser.json());
+
 function createTemplate(data){
 var title=data.title;
 var date=data.date;
@@ -68,6 +69,22 @@ function hash(input){
 app.get('/hash/:input',function(req,res){
     var hashedString=hash(req.params.input);
     res.send(hashedString);
+});
+
+app.post('/create-user',function(req,res){
+   //username,passwrd
+   //JSON 
+   var username=req.body.username;
+   var password=req.body.password;
+   var salt=crypto.getRandomBytes(128).toString('hex');
+   var dbString=hash(password,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+   if(err){
+       res.status(500).send(err.toString());
+   }else{
+       res.send('User successfully created: '+ username);
+   }    
+   });
 });
 
 var pool=new Pool(config);
